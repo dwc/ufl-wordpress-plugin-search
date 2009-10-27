0a4eb75d24247343ff634a9b3a0b08efb5e6fccd
danieltwc <danieltwc@36aa8936-c2fd-11de-a8ef-132414f8baa6>
2009-10-27 14:59:06 +0000

    Reorganize repository to follow standard layout

---
+++ controllers/class.UfSearchController.php
@@ -0,0 +1,35 @@
+<?php
+require_once(UF_PLUGIN_FRAMEWORK_LIBRARY . '/class.UfController.php');
+
+
+if (! class_exists('UfSearchController')) {
+	class UfSearchController extends UfController {
+		var $sources = array();
+		var $default = 'this';
+
+		function UfSearchController($sources, $default) {
+			$this->sources = $sources;
+			$this->default = $default;
+
+			$this->{get_parent_class(__CLASS__)}();
+		}
+
+		function handle_search_action() {
+			$source = $_REQUEST['source'];
+			$query = $_REQUEST['query'];
+
+			$sources = $this->sources;
+			$default = $this->default;
+
+			$default_source = $sources[$default];
+			$selected_source = ($sources[$source]) ? $sources[$source] : $default_source;
+			if ($selected_source) {
+				$selected_source->search($query);
+			}
+			else {
+				die('Could not determine source');
+			}
+		}
+	}
+}
+?>
---
+++ uf-search.php
@@ -0,0 +1,74 @@
+<?php
+/*
+Plugin Name: UF Search
+Version: 2.0
+Plugin URI: http://www.webadmin.ufl.edu/
+Description: Handling of the search form on WordPress sites at UF. Requires the <code>uf-plugin-framework</code> plugin.
+Author: Daniel Westermann-Clark
+Author URI: http://dev.webadmin.ufl.edu/~dwc/
+*/
+
+define('UF_SEARCH_PLUGIN_BASE', dirname(__FILE__) . '/');
+
+// Load the plugin after the framework
+add_action('plugins_loaded', 'uf_search_plugins_loaded');
+
+
+$uf_search_plugin = null;
+function uf_search_plugins_loaded() {
+	global $uf_search_plugin;
+
+	require_once('models/class.UfSearchSource.php');
+	$sources = array(
+		'this'      => new UfSearchSource(get_option('blogname'), get_option('siteurl') . '/index.php', 's'),
+		'web'       => new UfSearchSource('UF Web with Google', 'http://search.ufl.edu/web', 'query'),
+		'phonebook' => new UfSearchSource('UF Phonebook', 'http://phonebook.ufl.edu/people/search', 'query'),
+	);
+
+	require_once('plugins/class.UfSearchPlugin.php');
+	$uf_search_plugin = new UfSearchPlugin('Search', __FILE__, $sources);
+}
+
+/*
+ * Workaround WordPress' braindead lack of stripslashes.
+ */
+function uf_search_query() {
+	$query = '';
+
+	if (function_exists('get_search_query')) {
+		$query = get_search_query();
+	}
+	else {
+		$query = stripslashes(get_query_var('s'));
+	}
+
+	return $query;
+}
+
+function uf_search_uri($query) {
+	global $wp_rewrite;
+
+	$search_uri = '';
+
+	$search_permastruct = $wp_rewrite->get_search_permastruct();
+	if ($search_permastruct) {
+		$search_uri = get_bloginfo('url') . '/' . str_replace('%search%', urlencode($query), $search_permastruct);
+	}
+	else {
+		$search_uri = get_bloginfo('url') . '/index.php?s=' . urlencode($query);
+	}
+
+	return $search_uri;
+}
+
+function uf_search_sources() {
+	global $uf_search_plugin;
+
+	$sources = array();
+	if ($uf_search_plugin) {
+		$sources = $uf_search_plugin->sources;
+	}
+
+	return $sources;
+}
+?>
